811ef935bc17228081c0df98788070f4c7ffb7f8
ilayc <ilaycohen98@gmail.com>
2024-04-13 20:48:17 +0300

    def right mouse

--- server/serverControl.py
+++ server/serverControl.py
@@ -3,30 +3,41 @@ import win32api
 from screeninfo import get_monitors
 from vidstream import StreamingServer
 
-# mouse_keys =[0]*2
-state_left = win32api.GetKeyState(0x01)  # Left button down = 0 or 1. Button up = -127 or -128
-state_right = win32api.GetKeyState(0x02)  # Right button down = 0 or 1. Button up = -127 or -128
-# mouse_keys[0] = state_left
+mouse_keys = [win32api.GetKeyState(0x01), win32api.GetKeyState(0x02)]
 
 def left_button_pressed(state_left):
-
     a = win32api.GetKeyState(0x01)
-    b = win32api.GetKeyState(0x02)
-    left_button_pressed = False
+    left_button_got_pressed = False
+
     if a != state_left:  # Button state changed
         state_left = a
         print(a)
         if a < 0:
             print('Left Button Pressed')
-            left_button_pressed = True
+            left_button_got_pressed = True
         else:
             print('Left Button Released')
-            left_button_pressed = False
-    return left_button_pressed
+            left_button_got_pressed = False
 
+    return left_button_got_pressed
+
+def right_button_pressed(state_right):
+    b = win32api.GetKeyState(0x02)
+    right_button_got_pressed = False
+
+    if b != state_right:  # Button state changed
+        state_right = b
+        print(b)
+        if b < 0:
+            print('Right Button Pressed')
+            right_button_got_pressed = True
+        else:
+            print('Right Button Released')
+            right_button_got_pressed = False
+
+    return right_button_got_pressed
 
 def get_your_screen_resolution():
-    # Get the screen resolution of the primary monitor
     monitors = get_monitors()
 
     if monitors:
@@ -35,51 +46,46 @@ def get_your_screen_resolution():
         height = primary_monitor.height
         return width, height
     else:
-        # Default values if no monitors are found
-        return 1920, 1080  # Update with your default values
-
+        return 1920, 1080  # Default values
 
 def share_screen(HOST, PORT, client_socket, client_address):
-    print(f'got in')
-    width, height=get_your_screen_resolution()[0],get_your_screen_resolution()[1]
+    print(f'Got in')
+    width, height = get_your_screen_resolution()
     client_socket.send(f'{width},{height},1'.encode())
+
     host = StreamingServer(HOST, PORT-1)
     host.start_server()
 
     data = client_socket.recv(1024).decode().split(",")
     width_client, height_client = int(data[0]), int(data[1])
-    print(width_client,height_client)
-    print(width,height)
-    ratio_width=width_client/width
-    ratio_height=height_client/height
+    print(width_client, height_client)
+    print(width, height)
+    ratio_width = width_client / width
+    ratio_height = height_client / height
 
     while True:
         try:
             currentMouseX, currentMouseY = pyautogui.position()
-            # print("current mouse:",currentMouseX,currentMouseY)
+            mouse_x = int(currentMouseX * ratio_width)
+            mouse_y = int(currentMouseY * ratio_height) - 20
 
-
-            mouse_x=int(currentMouseX*ratio_width)
-            mouse_y=int(currentMouseY*ratio_height)-20
-
-            # print("mouse on client:", mouse_x, mouse_y)
-            # Check if the left mouse button is pressed
-            if left_button_pressed(state_left):
-                print("on click")
-                # Send message with additional information when the left button is pressed
+            if left_button_pressed(mouse_keys[0]):
+                print("Left click")
                 client_socket.send(f'{mouse_x},{mouse_y},1'.encode())
-                print("sent True")
+                print("Sent True")
+            elif right_button_pressed(mouse_keys[1]):
+                print("Right click")
+                client_socket.send(f'{mouse_x},{mouse_y},2'.encode())
             else:
                 client_socket.send(f'{mouse_x},{mouse_y},0'.encode())
-                # print("sent False")
 
             res = client_socket.recv(1024).decode()
-            # print(res)
 
         except Exception as e:
             print(f"Error in share_screen: {e}")
             break
 
-
     host.stop_server()
     print("Screen sharing stopped")
+
+# If this code is meant to be part of a larger program, make sure to integrate it properly with your existing codebase.
--- server/servermain.py
+++ server/servermain.py
@@ -6,17 +6,20 @@ import threading
 import time
 import serverControl
 
-HOST = '10.100.102.32'
+HOST = '172.20.137.79'
 PORT = 4444
 
+users_list = []
 
-users_list=[]
 
 def add_user(users_list, client_socket, client_address):
     users_list.append((client_socket, client_address))
+
+
 def check_user_can_controlled(users_list, client_socket, client_address):
-    if users_list[0]== (client_socket, client_address): return True
-    return False
+    return users_list and users_list[0] == (client_socket, client_address)
+
+
 def remove_user(users_list, client_socket, client_address):
     users_list.remove((client_socket, client_address))
 
@@ -28,61 +31,61 @@ def login(data):
 
 def handle_client(client_socket, client_address):
     print(f"Accepted connection from {client_address}")
-    # pyautogui.moveTo(100, 150)
+
     try:
-        flag=True
-        while True:
-            if not flag:
-                print('close flag=false ')
-                break
-            # Get username and password from client
+        flag = True
+        while flag:
             data = client_socket.recv(1024)
             if not data:
-                print('close no data')
+                print('No data received, closing connection')
                 break
+
             print(f"Received from {client_address}: {data.decode('utf-8')}")
-            # Check if username and password are correct
+
             if login(data):
-                while True:
-                    if not flag:
-                        break
-                    totp_code=tp.auth_code()
-                    code=totp_code[1]
-                    num=totp_code[0]
-                    client_socket.send(f'successful login, please enter the code: {code} to accept control by server'.encode())
-                    totp_check=tp.auth_check(client_socket, client_address, num)
-                    if totp_check=="exit":
-                        flag=False
+                while flag:
+                    totp_code = tp.auth_code()
+                    code = totp_code[1]
+                    num = totp_code[0]
+
+                    client_socket.send(
+                        f'successful login, please enter the code: {code} to accept control by server'.encode())
+
+                    totp_check = tp.auth_check(client_socket, client_address, num)
+
+                    if totp_check == "exit":
+                        flag = False
                         break
                     elif totp_check:
                         client_socket.send("best".encode())
                         time.sleep(2)
                         print('good code by client')
+
                         add_user(users_list, client_socket, client_address)
+
                         while not check_user_can_controlled(users_list, client_socket, client_address):
                             continue
+
                         if check_user_can_controlled(users_list, client_socket, client_address):
-                            serverControl.share_screen(HOST,PORT, client_socket, client_address)
-                        print("sharing screen")
-                        flag=False
+                            serverControl.share_screen(HOST, PORT, client_socket, client_address)
+
+                        print("Sharing screen")
+                        flag = False
                     else:
                         print('bad code by client, close connection')
                         client_socket.send("bad".encode())
-                        flag=False
-            # if not correct username or password it returns to the start and get again username, password from client
+                        flag = False
             else:
                 client_socket.send("bad".encode())
     except Exception as e:
         print(f"Error handling client: {e}")
-
     finally:
-        # if client wrote exit or no data it will close the connection with him and the server
         print(f"Connection from {client_address} closed.")
         client_socket.close()
         remove_user(users_list, client_socket, client_address)
 
+
 def start_server():
-    # Create a server socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((HOST, PORT))
     server_socket.listen()
@@ -93,6 +96,7 @@ def start_server():
         client, addr = server_socket.accept()
         client_handler = threading.Thread(target=handle_client, args=(client, addr))
         client_handler.start()
+
 
 if __name__ == "__main__":
     start_server()
